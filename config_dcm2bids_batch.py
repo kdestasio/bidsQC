e8039bafc24f21336e2d3d3d4943505c6c3ec7ca
kdestasio <kdestasio@gmail.com>
2018-04-29 12:30:22 -0700

    make study config generic

--- config_dcm2bids_batch.py
+++ config_dcm2bids_batch.py
@@ -12,7 +12,7 @@ dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", stu
 # Set directories
 niidir = os.path.join(os.sep, "projects", group, "shared", study, "bids_data") # Where the niftis will be put
 codedir = os.path.join(os.sep, "projects", group, "shared", study, "REV_scripts", "org", gitrepo)  # Contains subject_list.txt, config file, and dcm2bids_batch.py
-configfile = os.path.join(codedir, study + "_config.json")  # path to and name of config file
+configfile = os.path.join(codedir, "study_config.json")  # path to and name of config file
 image = os.path.join(os.sep, "projects", group, "shared", "containers", "Dcm2Bids-master.simg")
 logdir = os.path.join(codedir, "logs_dcm2bids")
 
@@ -20,4 +20,4 @@ outputlog = os.path.join(logdir, "outputlog_dcmn2bids" + datetime.now().strftime
 errorlog = os.path.join(logdir, "errorlog_dcm2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
 
 # Source the subject list (needs to be in your current working directory)
-subjectlist = "subject_list_test.txt"
+subjectlist = "subject_list.txt"
