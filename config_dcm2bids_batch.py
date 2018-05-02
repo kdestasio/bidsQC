7d10706f707e059bfa0ed2854b80b60c3cf7bacf
kdestasio <kdestasio@gmail.com>
2018-05-01 18:05:40 -0700

    Add option to run locally

--- config_dcm2bids_batch.py
+++ config_dcm2bids_batch.py
@@ -1,17 +1,34 @@
+# This script will convert all of the dicoms in the sourcedir
+# for participant directories that are listed in the subject_list.txt file.
+# Niftis will be renamed and put into BIDS structure using the dcm2Bids package
+#
+# See the dcm2Bids repo for instructions to create the config file:
+# https://github.com/cbedetti/Dcm2Bids
+#
+# More detailed instructions on san wiki:
+# https://uosanlab.atlassian.net/wiki/spaces/SW/pages/44269646/Convert+DICOM+to+BIDS
+#
+# In your current directory, you will need:
+#       - dcm2bids_batch.py
+#       - subject_list.txt
+#       - study_config.json
+
+
 # Configuration file for dcm2bids_batch.py
 import os
 from datetime import datetime
 
+
 # Set study info (may need to change for your study)
 group = "sanlab"
 study = "REV"
+parent_dir = "dcm2bids" # Where the scripts live
 
-gitrepo = "dcm2bids" # Where the scripts live
-dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", study)
 
 # Set directories
+dicomdir = os.path.join(os.sep, "projects", "lcni", "dcm", group, "Archive", study)
 niidir = os.path.join(os.sep, "projects", group, "shared", study, "bids_data") # Where the niftis will be put
-codedir = os.path.join(os.sep, "projects", group, "shared", study, "REV_scripts", "org", gitrepo)  # Contains subject_list.txt, config file, and dcm2bids_batch.py
+codedir = os.path.join(os.sep, "projects", group, "shared", study, "REV_scripts", "org", parent_dir)  # Contains subject_list.txt, config file, and dcm2bids_batch.py
 configfile = os.path.join(codedir, "study_config.json")  # path to and name of config file
 image = os.path.join(os.sep, "projects", group, "shared", "containers", "Dcm2Bids-master.simg")
 logdir = os.path.join(codedir, "logs_dcm2bids")
@@ -19,5 +36,10 @@ logdir = os.path.join(codedir, "logs_dcm2bids")
 outputlog = os.path.join(logdir, "outputlog_dcmn2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
 errorlog = os.path.join(logdir, "errorlog_dcm2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
 
+
 # Source the subject list (needs to be in your current working directory)
-subjectlist = "subject_list.txt"
+subjectlist = "subject_list_test.txt"
+
+
+# Run on local machine (run_local = True) or high performance cluster with slurm (run_local = False)
+run_local = True
--- dcm2bids_batch.py
+++ dcm2bids_batch.py
@@ -1,62 +1,103 @@
-# This script will convert all of the dicoms in the sourcedir
-# for participant directories that are listed in the subject_list.txt file.
-# Niftis will be renamed and put into BIDS structure using the dcm2Bids package
-#
-# See the dcm2Bids repo for instructions to create the config file:
-# https://github.com/cbedetti/Dcm2Bids
-#
-# More detailed instructions on san wiki:
-# https://uosanlab.atlassian.net/wiki/spaces/SW/pages/44269646/Convert+DICOM+to+BIDS
-#
-# In your current directory, you will need:
-#       - dcm2bids_batch.py
-#       - subject_list.txt
-#       - the study config file (e.g. REV_config.json)
-
 # Import libraries and configuration file
 import os
 import subprocess
 import config_dcm2bids_batch as cfg
 
-# Define a function to create files
-def touch(path):
-    """Create a new file"""
+# Main function
+def main():
+    """
+    Run the things.
+    """
+    folders_tocheck = cfg.logdir, cfg.niidir
+    check_dirs(folders_tocheck)
+    logfile_fullpaths = cfg.errorlog, cfg.outputlog
+    create_logfiles(logfile_fullpaths)
+    check_dicomdir(cfg.dicomdir)
+    batch_jobs(cfg.subjectlist, cfg.dicomdir, cfg.configfile, cfg.niidir)
+
+
+def batch_jobs(subject_list, dicomdir, configfile, niidir):
+    with open(subject_list) as file:
+        lines = file.readlines()  
+    for line in lines:
+        entry = line.strip()
+        subjectdir = entry.split(",")[0]
+        subject = entry.split(",")[1]
+        wave = entry.split(",")[2]
+        subjectpath = os.path.join(dicomdir, subjectdir)
+        if os.path.isdir(subjectpath):
+            write_to_outputlog(subjectdir + os.linesep)
+            if cfg.run_local:
+                batch_cmd = 'dcm2bids -d {subjectpath} -s {wave} -p {subject} -c {configfile} -o {niidir}  --forceDcm2niix --clobber'.format(subjectpath=subjectpath,  wave=wave, subject=subject, configfile=configfile,  niidir=niidir)
+            else:
+                batch_cmd = 'module load singularity; sbatch --job-name dcm2bids_{subjectdir} --partition=short --time 00:60:00 --mem-per-cpu=2G --cpus-per-task=1 -o {logdir}/{subjectdir}_dcm2bids_output.txt -e {logdir}/{subjectdir}_dcm2bids_error.txt --wrap="singularity run -B {dicomdir} -B {niidir} -B {codedir} {image} -d {subjectpath} -s {wave} -p {subject} -c {configfile} -o {niidir}  --forceDcm2niix --clobber"'.format(logdir=cfg.logdir, subjectdir=subjectdir, dicomdir=cfg.dicomdir, wave=wave, codedir=cfg.codedir, configfile=cfg.configfile, subject=subject, niidir=cfg.niidir, subjectpath=subjectpath, group=cfg.group, image=cfg.image)
+            subprocess.call([batch_cmd], shell=True)
+        else:
+            write_to_errorlog(subjectdir + os.linesep)
+
+
+def check_dicomdir(dicomdir):
+    if not os.path.isdir(dicomdir):
+        write_to_errorlog("Incorrect dicom directory specified")
+
+
+def touch(path:str):
+    """
+    Create a new file
+    
+    @type path:     string
+    @param path:    path to - including name of - file to be created
+    """
     with open(path, 'a'):
         os.utime(path, None)
 
-# Check/create log files and directories
-if not os.path.isdir(cfg.logdir):
-    os.mkdir(cfg.logdir)
-if not os.path.isfile(cfg.outputlog):
-    touch(cfg.outputlog)
-if not os.path.isfile(cfg.errorlog):
-    touch(cfg.errorlog)
 
-# Check directory dependencies
-if not os.path.isdir(cfg.niidir):
-    os.mkdir(cfg.niidir)
-if not os.path.isdir(cfg.dicomdir):
+def check_dirs(dir_fullpaths:list):
+    """
+    Check if a directory exists. If not, create it.
+
+    @type dir_fullpaths:        list
+    @param dir_fullpaths:       Paths to directorys to check
+    """
+    for dir_fullpath in dir_fullpaths:
+        if not os.path.isdir(dir_fullpath):
+            os.mkdir(dir_fullpath)
+
+            
+def create_logfiles(logfile_fullpaths:list):
+    """
+    Check if a logfile exists. If not, make one.
+
+    @type logfile_fullpaths:         list
+    @param logfile_fullpaths:        Paths to logfiles to check
+    """
+    for logfile_fullpath in logfile_fullpaths:
+        if not os.path.isfile(logfile_fullpath):
+            touch(logfile_fullpath)
+
+
+def write_to_outputlog(message):
+    """
+    Write a log message to the output log. Also print it to the terminal.
+
+    @type message:          string
+    @param message:         Message to be printed to the log
+    """
+    with open(cfg.outputlog, 'a') as logfile:
+        logfile.write(message + os.linesep)
+    print(message)
+
+
+def write_to_errorlog(message):
+    """
+    Write a log message to the error log. Also print it to the terminal.
+
+    @type message:          string
+    @param message:         Message to be printed to the log
+    """
     with open(cfg.errorlog, 'a') as logfile:
-        logfile.write("Incorrect dicom directory specified")
+        logfile.write(message + os.linesep)
+    print(message)
 
-# Convert the dicoms of each participant in the subject_list.txt file
-with open(cfg.subjectlist) as file:
-    lines = file.readlines()  
 
-# Split the subject list into participant ID and session number
-for line in lines:
-    entry = line.strip()
-    subjectdir = entry.split(",")[0]
-    subject = entry.split(",")[1]
-    wave = entry.split(",")[2]
-    subjectpath = os.path.join(cfg.dicomdir, subjectdir)
-    if os.path.isdir(subjectpath):
-        with open(cfg.outputlog, 'a') as logfile:
-            logfile.write(subjectdir + os.linesep)
-        # Create a job to submit to the HPC with sbatch
-        batch_cmd = 'module load singularity; sbatch --job-name dcm2bids_{subjectdir} --partition=short --time 00:60:00 --mem-per-cpu=2G --cpus-per-task=1 -o {logdir}/{subjectdir}_dcm2bids_output.txt -e {logdir}/{subjectdir}_dcm2bids_error.txt --wrap="singularity run -B {dicomdir} -B {niidir} -B {codedir} {image} -d {subjectpath} -s {wave} -p {subject} -c {configfile} -o {niidir}  --forceDcm2niix --clobber"'.format(logdir=cfg.logdir, subjectdir=subjectdir, dicomdir=cfg.dicomdir, wave=wave, codedir=cfg.codedir, configfile=cfg.configfile, subject=subject, niidir=cfg.niidir, subjectpath=subjectpath, group=cfg.group, image=cfg.image)
-        # Submit the job
-        subprocess.call([batch_cmd], shell=True)
-    else:
-        with open(cfg.errorlog, 'a') as logfile:
-            logfile.write(subjectdir + os.linesep)
+main()
