c3d44b3b4d4cf539739243b48ae272190f41084d
kdestasio <kdestasio@gmail.com>
2018-05-05 13:53:29 -0700

    run local or on hpc

--- dcm2bids_helper.py
+++ dcm2bids_helper.py
@@ -16,8 +16,10 @@ def main():
     if os.path.isdir(cfg.dicomdir):
         write_to_outputlog(cfg.test_subject + os.linesep)
         # Create a job to submit to the HPC with sbatch
-        cmd = 'module load singularity; sbatch --job-name helper_{test_subject} --partition=short --time 00:60:00 --mem-per-cpu=2G --cpus-per-task=1 -o {logdir}/{test_subject}_helper_output.txt -e {logdir}/{test_subject}_helper_error.txt --wrap="singularity exec -B {dicomdir} -B {parentdir} {image} dcm2bids_helper -d {dicomdir}/{test_subject} -o {logdir}"'.format(dicomdir=cfg.dicomdir, test_subject=cfg.test_subject, bidsdir=cfg.bidsdir, group=cfg.group, image=cfg.singularity_image, study=cfg.study, logdir=cfg.logdir)
-        # Submit the job
+        if cfg.run_local:
+            cmd = 'dcm2bids_helper -d {dicomdir}/{test_subject} -b {bidsdir}'.format(dicomdir=cfg.dicomdir, test_subject=cfg.test_subject, niidir=cfg.bidsdir)
+        else:
+            cmd = 'module load singularity; sbatch --job-name helper_{test_subject} --partition=short --time 00:60:00 --mem-per-cpu=2G --cpus-per-task=1 -o {logdir}/{test_subject}_helper_output.txt -e {logdir}/{test_subject}_helper_error.txt --wrap="singularity exec -B {dicomdir} -B {parentdir} {image} dcm2bids_helper -d {dicomdir}/{test_subject} -o {bidsdir}"'.format(dicomdir=cfg.dicomdir, test_subject=cfg.test_subject, bidsdir=cfg.bidsdir, group=cfg.group, image=cfg.singularity_image, study=cfg.study, logdir=cfg.logdir)
         subprocess.call([cmd], shell=True)
     else:
         write_to_errorlog(cfg.test_subject+os.linesep)
