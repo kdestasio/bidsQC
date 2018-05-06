f6b83f78ee222f2689b6cb7d8605659426cf1c19
kdestasio <kdestasio@gmail.com>
2018-05-06 14:46:19 -0700

    Remove excess path block

--- bidsQC/config_bidsQC.py
+++ bidsQC/config_bidsQC.py
@@ -14,19 +14,13 @@ study = 'CHIVES'
 # Set directories (Check these for your study)
 # These variables are used in the main script and need to be defined here. 
 # They need to exist prior to running the script.
-# logdir = os.path.join(os.getcwd(), 'logs_bidsQC')
-# bidsdir = os.path.join(os.sep, 'projects', group, 'shared', study, 'bids_data')
-# tempdir = os.path.join(bidsdir, 'tmp_dcm2bids')
-# outputlog = os.path.join(logdir, 'outputlog_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
-# errorlog = os.path.join(logdir, 'errorlog_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
-# derivatives = os.path.join(bidsdir, 'derivatives')
-
-bidsdir = os.path.join(os.sep, 'Users', 'kristadestasio', 'Desktop', 'bids_data')
-logdir = os.path.join(bidsdir, 'logs_bidsQC')
+logdir = os.path.join(os.getcwd(), 'logs_bidsQC')
+bidsdir = os.path.join(os.sep, 'projects', group, 'shared', study, 'bids_data')
 tempdir = os.path.join(bidsdir, 'tmp_dcm2bids')
-outputlog = os.path.join(logdir, 'outputlog_bidsQC_' + datetime.now().strftime('%Y%m%d-%H%M') + '.txt')
-errorlog = os.path.join(logdir, 'errorlog_bidsQC_' + datetime.now().strftime('%Y%m%d-%H%M') + '.txt')
+outputlog = os.path.join(logdir, 'outputlog_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+errorlog = os.path.join(logdir, 'errorlog_bidsQC' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
 derivatives = os.path.join(bidsdir, 'derivatives')
+
 
 # Create a dictionary (the thing below) for each timepoint in your study where the pairs are 'sequence_directory_name' : 'expected_number_runs'
 # Each unique version of a sequence gets its own entry, e.g. 'gng_acq-1' and 'gng_acq-2'
