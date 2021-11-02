2bc693e38888ec6bd1c89c47dd6d04e215a35dd7
kdestasio <kdesyasio@gmail.com>
2021-11-02 15:11:52 -0700

    Move rpaths that should not be changed out of configurable section. Change example names and add more comments to help the user understand what is needed.

--- qualityCheck/config_qualityCheck.py
+++ qualityCheck/config_qualityCheck.py
@@ -3,24 +3,32 @@ from datetime import datetime
 from sequence import Sequence
 from timepoint import TimePoint
 
-######################## CONFIGURABLE PART BELOW ########################
+# Required that should not be changed
+tempdir = os.path.join(pathToBidsFolder, 'tmp_dcm2bids')  # holding folder for undesired files
+outputlog = os.path.join(logdir, 'outputlog_qualityCheck' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+errorlog = os.path.join(logdir, 'errorlog_qualityCheck' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
+derivatives = os.path.join(pathToBidsFolder, 'derivatives')  # Where processed data will go
 
-# Set your Talapas user group
-group = "sanlab"
+######################## CONFIGURABLE PART BELOW ########################
 
 # Set directories (Check these for your study)
 # These variables are used in the main script and need to be defined here. 
 # They need to exist prior to running the qualityCheck.py
 pathToBidsFolder = os.path.join(os.sep, 'projects', "sanlab", 'shared', "studyName", 'bids_data')  # Where your subjects' nifti directories are
-logdir = os.path.join(pathToBidsFolder, 'logs_qualityCheck')  # Where log files will go
+logdir = os.path.join(os.getcwd, 'logs_qualityCheck')  # Log files will go in the folder from which this script is run
 
-# Create a dictionary (the thing below) for each timepoint in your study where the pairs are 'sequence_directory_name' : 'expected_number_runs'
-# Each unique version of a sequence gets its own entry, e.g. 'gng_acq-1' and 'gng_acq-2'
-sequence1 = Sequence('func', {'task1': 2, 'task2_acq-1':2, 'task2_acq-2':1, 'task3_acq-1':1, 'task3_acq-2':1})
+# Create a dictionary (the thing below) for each timepoint in your study.
+# The first entry after Sequence is the directory name
+# e.g. Sequence('func', ...) means the directory is named func
+# The key:value pairs within the curly {} braces are taskName:numberOfRuns
+# e.g. a task named stopsignal that the subject completed twice would be entered as {'stopsignal':2}
+sequence1 = Sequence('func', {'stopsignal': 2, 'react':1})
 sequence3 = Sequence('anat', {'T1w':1})
-sequence4 = Sequence('fmap', {'dir-ap':1, 'dir-pa':1})
-timepoint1 = TimePoint('ses-wave1', [sequence1, sequence3, sequence4])
-expected_timepoints = [timepoint1]
+sequence4 = Sequence('fmap', {'dir-ap':1, 'dir-pa':1})  # Example of fieldmap with multiple phase encoded directions
+sequence5 = Sequence("fmap", {"magnitude1":1, "magnitude2":1, "phasediff":1 }) # Example of fieldmap with phase-difference map and multiple magnitude images
+timepoint1 = TimePoint('ses-wave1', [sequence1, sequence3, sequence4])   # Create an entry for each timepoint in your study
+timepoint2 = TimePoint('ses-wave2', [sequence1, sequence5])  # Add as many timepoints as needed and fill it with the correct sequences
+expected_timepoints = [timepoint1, timepoint2]
 
 
 # Files g-zipped or not? 
@@ -28,16 +36,9 @@ expected_timepoints = [timepoint1]
 # A mixture won't work properly.
 gzipped = True
 
-
-# Do we want runs based on sequence order? Yes = True
-# This is for tasks that use counterbalancing (of e.g. stimulus blocks) anf for which we want to identify the content
-# with the 'acq-' label and order administered with the 'task-' label.
+# If you have different versions of the same task - e.g. stopsignal was run twice with different images
+# And those runs were named differently - e.g. stopsignalA and stopsignalB
+# You need them to be recognized as multiple runs of the same task. 
+# List them below to have the 'run-#' field appended to indicate the order in which they were run.
 order_sequences = True
-tasks_to_order = 'task1', 'task2'
-
-
-# Paths that are required and that should not be changed
-tempdir = os.path.join(pathToBidsFolder, 'tmp_dcm2bids')  # holding folder for undesired files
-outputlog = os.path.join(logdir, 'outputlog_qualityCheck' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
-errorlog = os.path.join(logdir, 'errorlog_qualityCheck' + datetime.now().strftime('%Y%m%d-%H%M%S') + '.txt')
-derivatives = os.path.join(pathToBidsFolder, 'derivatives')  # Where processed data will go
+tasks_to_order = 'stopsignal'    # comma seperated if multiple
