c64206a463a8c89390b0d235a4ddeaee090e9a56
kdestasio <kdesyasio@gmail.com>
2021-12-16 13:19:53 -0800

    Seperate out config file definition. Update commented wording.

--- conversion/config_dcm2bids_batch.py
+++ conversion/config_dcm2bids_batch.py
@@ -5,20 +5,21 @@ from datetime import datetime
 
 # Set your Talapas user group
 group = "sanlab"
-partition = "ctn" # Set to NA if running locally
-dcm2bidsImageName = "dcm2bids_latest_2021-11-18.sif" # Set to NA if running locally
+partition = "ctn" # ignored if running locally
+dcm2bidsImageName = "dcm2bids_latest_2021-11-18.sif" # ignored if running locally
+config_file = "study_config.json"
 
 # Set directories
-# These variables are used in the main script and need to be defined here.
+# These variables are used in the main script and mustbe defined here.
 # They need to exist prior to running the script
 pathToStudyFolder = os.path.join(os.sep, "projects", "sanlab", "shared", "studyName") # folder that contains pathToBidsFolder and pathToConversionFolder
 pathToDicomFolder = os.path.join(os.sep, "projects", "lcni", "dcm", "sanlab", "Berkman", "REV")
 pathToConversionFolder = os.path.join(pathToStudyFolder, "bidsQC", "conversion")  # Contains subject_list.txt, config file, and dcm2bids_batch.py
-pathToConfigFile = os.path.join(pathToConversionFolder, "study_config.json")  # path to and name of config file
+pathToConfigFile = os.path.join(pathToConversionFolder, config_file)  # Don't cahnge this one
 singularity_image = os.path.join(os.sep, "projects", "sanlab", "shared", "containers", dcm2bidsImageName) # Set equal to "NA" if you are running the script locally
 
 # These variables are also used in the main script and need to be defined here.
-# If they don't exist, they will be created by the script
+# If the directories don't exist, they will be created by the script
 pathToBidsFolder = os.path.join(os.sep, "projects", group, "shared", "studyName", "bids_data") # Where the niftis will be put
 logdir = os.path.join(pathToBidsFolder, "logs_dcm2bids")
 outputlog = os.path.join(logdir, "outputlog_dcmn2bids" + datetime.now().strftime("%Y%m%d-%H%M") + ".txt")
