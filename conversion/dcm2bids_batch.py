3e7eebf4175ce71f6baa617c79ee80ec57626aff
kdestasio <kdesyasio@gmail.com>
2021-12-28 14:26:54 -0800

    Import time and add 10 seconds of sleep to job submission loop to address issue of failed jobs on University of Oregon high performance cluster.

--- conversion/dcm2bids_batch.py
+++ conversion/dcm2bids_batch.py
@@ -1,6 +1,7 @@
 # Import libraries and configuration file
 import os
 import subprocess
+import time
 import config_dcm2bids_batch as cfg
 
 # Main function
@@ -26,6 +27,7 @@ def batch_jobs(subject_list, path_dicoms, path_config, path_bidsdata):
         wave = entry.split(",")[2]
         subjectpath = os.path.join(path_dicoms, subjectdir)
         if os.path.isdir(subjectpath):
+            time.sleep(10)
             write_to_outputlog(subjectdir + os.linesep)
             if cfg.run_local:
                 batch_cmd = 'dcm2bids -d {subjectpath} -s {wave} -p {subject} -c {path_config} -o {path_bidsdata}  --forceDcm2niix --clobber'.format(subjectpath=subjectpath,  wave=wave, subject=subject, path_config=path_config,  path_bidsdata=path_bidsdata)
